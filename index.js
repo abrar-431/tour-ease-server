ebd76e58668177944f3ba096a5978d6a29e56046
unknown <abrarh1520444@gmail.com>
2024-04-28 23:50:54 +0600

    data in database created

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const spotCollection = client.db("spotsDB").collection('spots');
+
+    app.post('/spots', async(req, res)=>{
+      const spot = req.body;
+      console.log(spot);
+      const result = await spotCollection.insertOne(spot);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");

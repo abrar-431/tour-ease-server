bba7c0f29b45616f96c2d52fde3ad89be92fb163
unknown <abrarh1520444@gmail.com>
2024-04-29 20:16:56 +0600

    countries collection added

--- index.js
+++ index.js
@@ -26,9 +26,17 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const spotCollection = client.db("spotsDB").collection('spots');
+    const countryCollection = client.db("spotsDB").collection('countries');
 
     app.get('/spots', async(req, res)=>{
       const cursor  = spotCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // Countries
+    app.get('/countries', async(req, res)=>{
+      const cursor  = countryCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     })

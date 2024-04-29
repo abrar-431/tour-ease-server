0eb4f4a2a42b34465a146b7e08f22265587da3ff
unknown <abrarh1520444@gmail.com>
2024-04-29 12:12:11 +0600

    delete operation added

--- index.js
+++ index.js
@@ -53,6 +53,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/spots/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await spotCollection.deleteOne(query);
+      res.send(result);
+    })
+
     app.put('/update/:id', async(req, res)=>{
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)};
